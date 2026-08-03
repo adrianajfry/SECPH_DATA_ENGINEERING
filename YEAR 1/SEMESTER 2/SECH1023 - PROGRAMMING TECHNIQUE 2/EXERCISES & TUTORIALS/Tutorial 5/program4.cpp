f6704d137f75776b8fd8fedb388b82fc93df7fc8
adrianajfry <148573812+adrianajfry@users.noreply.github.com>
2026-08-03 16:15:46 +0800

    Add files via upload

---
+++ YEAR 1/SEMESTER 2/SECH1023 - PROGRAMMING TECHNIQUE 2/EXERCISES & TUTORIALS/Tutorial 5/program4.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+// Task 1: Define the function lowercase that converts a string to lower case
+string lowercase(string s)
+{
+ //_____________________
+
+}
+
+
+// Task 2: Define the function textToNumber that converts a number in word to its integer value
+int textToNumber(string txt)
+{
+ //_____________________
+}
+
+int main()
+{
+	// Task 3a: Read user input
+	string text;
+	cout <<"What is: ";
+	//_____________________
+	
+	// Task 3b: Convert the user input to lower case
+	//_____________________
+	
+	int len = text.length();
+	
+	
+	// Task 4: Extract words of the first and second operands as well as the operator
+	int pos1 = text.find(' ');
+	//_____________________
+	//_____________________
+	
+
+	string operand1 = text.substr(0,pos1);
+	//_____________________
+	//_____________________
+	
+
+	// Task 5: Convert the operands from words to integers
+	//_____________________
+	//_____________________
+	
+	
+	// Task 6: Perform the arithmetic calculation accordingly, i.e., addition or subtraction
+	int result;
+	//_____________________
+	//_____________________
+	
+
+	// Task 7: Convert the result from integer to word
+	//_____________________
+	//_____________________
+    //_____________________	
+	
+	// Task 8: Print the answer
+	//_____________________
+	
+	cout << endl << endl;
+	system("pause"); //! remove this line if you are using Dev C++
+	return 0;
+}
